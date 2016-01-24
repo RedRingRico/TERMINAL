5692969ee2bf38a700ed2c9e7b5372a6de58d235
Rico <rico@redringrico.com>
2016-01-24 03:36:13 +0000

    Added GD-ROM build files

---
+++ build.bat
@@ -0,0 +1,10 @@
+cd Source
+make
+cd ..\GDROM
+copy ..\Bin\Dreamcast\1ST_READ.BIN .
+ipmaker_cmd ip_config.json
+copy %KATANA_ROOT%\Submit\Warning\US_warn.da warning.da
+%KATANA_ROOT%\Utl\Dev\CDCraft\crfgdc -bld=BUILD.SCR,dsk -benv=imf1
+cim2gdi TERMINAL.cim
+cd ..
+
