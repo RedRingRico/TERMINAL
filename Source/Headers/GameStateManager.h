8f00d32f60569db8b0d569132ebff6a4f6f15c21
Rico Tyrell <rico@redringrico.com>
2016-04-06 19:47:01 +0100

    Added the ability to change the current running game state

--- Source/Headers/GameStateManager.h
+++ Source/Headers/GameStateManager.h
@@ -2,6 +2,7 @@
 #define __TERMINAL_GAMESTATEMANAGER_H__
 
 #include <GameState.h>
+#include <Memory.h>
 
 typedef struct _tagGAMESTATE_REGISTRY
 {
@@ -14,11 +15,22 @@ typedef struct _tagGAMESTATE_REGISTRY
 
 typedef struct _tagGAMESTATE_MANAGER
 {
+	PMEMORY_BLOCK		pMemoryBlock;
 	GAMESTATE_REGISTRY	*pRegistry;
+	STACK				GameStateStack;
 }GAMESTATE_MANAGER,*PGAMESTATE_MANAGER;
 
-int GSM_Initialise( PGAMESTATE_MANAGER p_pGameStateManager );
+int GSM_Initialise( PGAMESTATE_MANAGER p_pGameStateManager,
+	PMEMORY_BLOCK p_pMemoryBlock );
 void GSM_Terminate( PGAMESTATE_MANAGER p_pGameStateManager );
+
+int GSM_ChangeState( PGAMESTATE_MANAGER p_pGameStateManager,
+	const char *p_pStateName );
+int GSM_PushState( PGAMESTATE_MANAGER p_pGameStateManager,
+	const char *p_pStateName );
+int GSM_PopState( PGAMESTATE_MANAGER p_pGameStateManager );
+
+int GSM_Exit( void );
 
 int GSM_RegisterGameState( PGAMESTATE_MANAGER p_pGameStateManager,
 	const char *p_pGameStateName, PGAMESTATE p_pGameState );
--- Source/Source/GameStateManager.c
+++ Source/Source/GameStateManager.c
@@ -2,10 +2,24 @@
 #include <Log.h>
 #include <string.h>
 
-int GSM_Initialise( PGAMESTATE_MANAGER p_pGameStateManager )
+int GSM_Initialise( PGAMESTATE_MANAGER p_pGameStateManager,
+	PMEMORY_BLOCK p_pMemoryBlock )
 {
 	p_pGameStateManager->pRegistry = NULL;
 	p_pGameStateManager->pRegistry->pNext = NULL;
+
+	if( STK_Initialise( &p_pGameStateManager->GameStateStack, p_pMemoryBlock,
+		10, sizeof( GAMESTATE ), 0,	"Game State Stack" )  != 0 )
+	{
+		LOG_Debug( "GSM_Initialise <ERROR> Failed to allocate memory for the "
+			"game state stack\n" );
+
+		return 1;
+	}
+
+	p_pGameStateManager->pMemoryBlock = p_pMemoryBlock;
+
+	return 0;
 }
 
 void GSM_Terminate( PGAMESTATE_MANAGER p_pGameStateManager )
@@ -21,6 +35,44 @@ void GSM_Terminate( PGAMESTATE_MANAGER p_pGameStateManager )
 
 		pRegistryItr = pNext;
 	}
+
+	STK_Terminate( &p_pGameStateManager->GameStateStack );
+}
+
+int GSM_ChangeState( PGAMESTATE_MANAGER p_pGameStateManager,
+	const char *p_pStateName )
+{
+	GAMESTATE GameState;
+	PGAMESTATE_REGISTRY pRegistryItr;
+
+	/* Pop all states */
+	while( STK_GetCount( &p_pGameStateManager->GameStateStack ) )
+	{
+		STK_Pop( &p_pGameStateManager->GameStateStack, NULL );
+	}
+
+	pRegistryItr = p_pGameStateManager->pRegistry;
+
+	while( pRegistryItr != NULL )
+	{
+		if( strcmp( p_pStateName, pRegistryItr->pName ) == 0 )
+		{
+			break;
+		}
+		pRegistryItr = pRegistryItr->pNext;
+	}
+
+	if( pRegistryItr == NULL )
+	{
+		LOG_Debug( "GSM_ChangeState <ERROR> Could not locate game state \""
+			"%s\"\n", p_pStateName );
+
+		return 1;
+	}
+
+	STK_Push( &p_pGameStateManager->GameStateStack, &pRegistryItr->GameState );
+
+	return 0;
 }
 
 int GSM_RegisterGameState( PGAMESTATE_MANAGER p_pGameStateManager,
@@ -35,7 +87,7 @@ int GSM_RegisterGameState( PGAMESTATE_MANAGER p_pGameStateManager,
 		if( strcmp( p_pGameStateName, pRegistryItr->pName ) == 0 )
 		{
 			LOG_Debug( "GSM_RegisterGameState <ERROR> Attempting to "
-				"re-register game state \"%s\"", p_pGameStateName );
+				"re-register game state \"%s\"\n", p_pGameStateName );
 
 			return 1;
 		}
