345dd8e3400197a16ceba70418228e9b26fbed10
Rico Tyrell <rico@redringrico.com>
2016-04-10 19:46:00 +0100

    The menu can be rendered and iterated through
    |SOURCE|
    [Menu]
    * Added an option to set the alignment of the text in the menu
    * The menu has a default colour for the text
    * MNU_Render can render the menu with the desired spacing at the coordinates
      passed in
    * MNU_Select{Previous,Next}MenuItem allow for iterating through the menu items
    * MNU_GetSelectedMenuItem returns the currently selected menu item, which has
      been manipulated via the MNU_Select{Previous,Next}MenuItem functions
    * Currently, the string based method of highlighting menu items is supported

--- Source/Headers/Menu.h
+++ Source/Headers/Menu.h
@@ -1,6 +1,7 @@
 #ifndef __TERMINAL_MENU_H__
 #define __TERMINAL_MENU_H__
 
+#include <Text.h>
 #include <shinobi.h>
 #include <kamui2.h>
 
@@ -11,6 +12,21 @@ typedef enum
 	SELECTION_HIGHLIGHT_TYPE_BOX,
 	SELECTION_HIGHLIGHT_TYPE_STRING
 }SELECTION_HIGHLIGHT_TYPE;
+
+typedef enum
+{
+	SELECTION_HIGHLIGHT_STRING_POSITION_LEFT,
+	SELECTION_HIGHLIGHT_STRING_POSITION_RIGHT,
+	SELECTION_HIGHLIGHT_STRING_POSITION_TOP,
+	SELECTION_HIGHLIGHT_STRING_POSITION_BOTTOM
+}SELECTION_HIGHLIGHT_STRING_POSITION;
+
+typedef enum
+{
+	MENU_ITEM_ALIGNMENT_LEFT,
+	MENU_ITEM_ALIGNMENT_RIGHT,
+	MENU_ITEM_ALIGNMENT_CENTRE
+}MENU_ITEM_ALIGNMENT;
 
 typedef struct _tagMENU_ITEM
 {
@@ -23,8 +39,7 @@ typedef struct _tagSELECTION_HIGHLIGHT
 {
 	SELECTION_HIGHLIGHT_TYPE	Type;
 	Uint32						Flags;
-	KMPACKEDARGB				TextColour;
-	KMPACKEDARGB				TextHighlightColour;
+	KMPACKEDARGB				HighlightColour;
 	float						TextPulseRate;
 	float						TextHighlightPulseRate;
 }SELECTION_HIGHLIGHT,*PSELECTION_HIGHLIGHT;
@@ -37,8 +52,9 @@ typedef struct _tagSELECTION_HIGHLIGHT_BOX
 
 typedef struct _tagSELECTION_HIGHLIGHT_STRING
 {
-	SELECTION_HIGHLIGHT	Base;
-	char				*pString;
+	SELECTION_HIGHLIGHT					Base;
+	char								*pString;
+	SELECTION_HIGHLIGHT_STRING_POSITION	Position;
 }SELECTION_HIGHLIGHT_STRING,*PSELECTION_HIGHLIGHT_STRING;
 
 typedef struct _tagMENU
@@ -46,12 +62,23 @@ typedef struct _tagMENU
 	PMENU_ITEM				pMenuItems;
 	size_t					MenuItemCount;
 	size_t					SelectedMenuItem;
-	PSELECTION_HIGHLIGHT	*pSelectionHighlight;
+	PSELECTION_HIGHLIGHT	pSelectionHighlight;
+	PGLYPHSET				pGlyphSet;
+	KMPACKEDARGB			TextColour;
+	MENU_ITEM_ALIGNMENT		MenuItemAlignment;
 }MENU,*PMENU;
 
 int MNU_Initialise( PMENU p_pMenu, PMENU_ITEM p_pMenuItems,
-	size_t p_MenuItemCount, PSELECTION_HIGHLIGHT p_pSelectionHighlight );
+	size_t p_MenuItemCount, PSELECTION_HIGHLIGHT p_pSelectionHighlight,
+	PGLYPHSET p_pGlyphSet, KMPACKEDARGB p_TextColour,
+	MENU_ITEM_ALIGNMENT p_MenuItemAlignment );
 void MNU_Terminate( PMENU p_pMenu );
+
+void MNU_SelectNextMenuItem( PMENU p_pMenu );
+void MNU_SelectPreviousMenuItem( PMENU p_pMenu );
+PMENU_ITEM MNU_GetSelectedMenuItem( PMENU p_pMenu );
+
+void MNU_Render( PMENU p_pMenu, float p_Spacing, float p_X, float p_Y );
 
 #endif /* __TERMINAL_MENU_H__ */
 
--- Source/Source/Menu.c
+++ Source/Source/Menu.c
@@ -2,7 +2,9 @@
 #include <Log.h>
 
 int MNU_Initialise( PMENU p_pMenu, PMENU_ITEM p_pMenuItems,
-	size_t p_MenuItemCount, PSELECTION_HIGHLIGHT p_pSelectionHighlight )
+	size_t p_MenuItemCount, PSELECTION_HIGHLIGHT p_pSelectionHighlight,
+	PGLYPHSET p_pGlyphSet, KMPACKEDARGB p_TextColour,
+	MENU_ITEM_ALIGNMENT p_MenuItemAlignment )
 {
 	switch( p_pSelectionHighlight->Type )
 	{
@@ -17,10 +19,24 @@ int MNU_Initialise( PMENU p_pMenu, PMENU_ITEM p_pMenuItems,
 		}
 		case SELECTION_HIGHLIGHT_TYPE_STRING:
 		{
+			PSELECTION_HIGHLIGHT_STRING pHighlightString;
+			PSELECTION_HIGHLIGHT_STRING pHighlightStringParam;
+
 			p_pMenu->pSelectionHighlight =
 				syMalloc( sizeof( SELECTION_HIGHLIGHT_STRING ) );
 			memcpy( p_pMenu->pSelectionHighlight, p_pSelectionHighlight,
 				sizeof( SELECTION_HIGHLIGHT_STRING ) );
+			pHighlightString =
+				( PSELECTION_HIGHLIGHT_STRING )p_pMenu->pSelectionHighlight;
+
+			pHighlightStringParam =
+				( PSELECTION_HIGHLIGHT_STRING )p_pSelectionHighlight;
+			pHighlightString->pString = syMalloc( strlen(
+				pHighlightStringParam->pString ) + 1 );
+			strncpy( pHighlightString->pString, pHighlightStringParam->pString,
+				strlen( pHighlightStringParam->pString ) );
+			pHighlightString->pString[
+				strlen( pHighlightStringParam->pString ) ] = '\0';
 
 			break;
 		}
@@ -63,6 +79,9 @@ int MNU_Initialise( PMENU p_pMenu, PMENU_ITEM p_pMenuItems,
 	}
 
 	p_pMenu->SelectedMenuItem = 0;
+	p_pMenu->pGlyphSet = p_pGlyphSet;
+	p_pMenu->TextColour = p_TextColour;
+	p_pMenu->MenuItemAlignment = p_MenuItemAlignment;
 
 	return 0;
 }
@@ -78,6 +97,202 @@ void MNU_Terminate( PMENU p_pMenu )
 		syFree( p_pMenu->pMenuItems );
 
 		p_pMenu->pMenuItems = pNext;
+	}
+}
+
+void MNU_SelectNextMenuItem( PMENU p_pMenu )
+{
+	if( p_pMenu->SelectedMenuItem == p_pMenu->MenuItemCount - 1 )
+	{
+		p_pMenu->SelectedMenuItem = 0;
+	}
+	else
+	{
+		++p_pMenu->SelectedMenuItem;
+	}
+}
+
+void MNU_SelectPreviousMenuItem( PMENU p_pMenu )
+{
+	if( p_pMenu->SelectedMenuItem == 0 )
+	{
+		p_pMenu->SelectedMenuItem = p_pMenu->MenuItemCount - 1;
+	}
+	else
+	{
+		--p_pMenu->SelectedMenuItem;
+	}
+}
+
+PMENU_ITEM MNU_GetSelectedMenuItem( PMENU p_pMenu )
+{
+	return &p_pMenu->pMenuItems[ p_pMenu->SelectedMenuItem ];
+}
+
+void MNU_Render( PMENU p_pMenu, float p_Spacing, float p_X, float p_Y )
+{
+	size_t Index;
+
+	for( Index = 0; Index < p_pMenu->MenuItemCount; ++Index )
+	{
+		if( Index == p_pMenu->SelectedMenuItem )
+		{
+			switch( p_pMenu->pSelectionHighlight->Type )
+			{
+				case SELECTION_HIGHLIGHT_TYPE_BOX:
+				{
+					break;
+				}
+				case SELECTION_HIGHLIGHT_TYPE_STRING:
+				{
+					float TextLength;
+					PSELECTION_HIGHLIGHT_STRING pHighlight =
+						( PSELECTION_HIGHLIGHT_STRING )
+							p_pMenu->pSelectionHighlight;
+
+					TXT_MeasureString( p_pMenu->pGlyphSet, pHighlight->pString,
+						&TextLength );
+
+					switch( p_pMenu->MenuItemAlignment )
+					{
+
+						case MENU_ITEM_ALIGNMENT_LEFT:
+						{
+							TXT_RenderString( p_pMenu->pGlyphSet,
+								&p_pMenu->pSelectionHighlight->HighlightColour,
+								p_X - TextLength, p_Y +
+									( float )p_pMenu->pGlyphSet->LineHeight *
+									( Index * p_Spacing ),
+								pHighlight->pString );
+
+							break;
+						}
+						case MENU_ITEM_ALIGNMENT_RIGHT:
+						{
+							float ItemLength;
+
+							TXT_MeasureString( p_pMenu->pGlyphSet,
+								p_pMenu->pMenuItems[ Index ].pName,
+								&ItemLength );
+
+							TXT_RenderString( p_pMenu->pGlyphSet,
+								&p_pMenu->pSelectionHighlight->HighlightColour,
+								p_X - ( TextLength + ItemLength ), p_Y +
+									( float )p_pMenu->pGlyphSet->LineHeight *
+									( Index * p_Spacing ),
+								pHighlight->pString );
+
+							break;
+						}
+						case MENU_ITEM_ALIGNMENT_CENTRE:
+						{
+							float ItemLength;
+
+							TXT_MeasureString( p_pMenu->pGlyphSet,
+								p_pMenu->pMenuItems[ Index ].pName,
+								&ItemLength );
+
+							TXT_RenderString( p_pMenu->pGlyphSet,
+								&p_pMenu->pSelectionHighlight->HighlightColour,
+								p_X - ( TextLength + ItemLength * 0.5f ), p_Y +
+									( float )p_pMenu->pGlyphSet->LineHeight *
+									( Index * p_Spacing ),
+								pHighlight->pString );
+
+							break;
+						}
+					}
+					break;
+				}
+			}
+
+			switch( p_pMenu->MenuItemAlignment )
+			{
+				case MENU_ITEM_ALIGNMENT_LEFT:
+				{
+					TXT_RenderString( p_pMenu->pGlyphSet,
+						&p_pMenu->pSelectionHighlight->HighlightColour,
+						p_X, p_Y + ( float )p_pMenu->pGlyphSet->LineHeight *
+							( Index * p_Spacing ),
+						p_pMenu->pMenuItems[ Index ].pName );
+
+					break;
+				}
+				case MENU_ITEM_ALIGNMENT_RIGHT:
+				{
+					float TextLength;
+
+					TXT_MeasureString( p_pMenu->pGlyphSet,
+						p_pMenu->pMenuItems[ Index ].pName, &TextLength );
+					TXT_RenderString( p_pMenu->pGlyphSet,
+						&p_pMenu->pSelectionHighlight->HighlightColour,
+						p_X - TextLength,
+						p_Y + ( float )p_pMenu->pGlyphSet->LineHeight *
+							( Index * p_Spacing ),
+						p_pMenu->pMenuItems[ Index ].pName );
+
+					break;
+				}
+				case MENU_ITEM_ALIGNMENT_CENTRE:
+				{
+					float TextLength;
+
+					TXT_MeasureString( p_pMenu->pGlyphSet,
+						p_pMenu->pMenuItems[ Index ].pName, &TextLength );
+					TXT_RenderString( p_pMenu->pGlyphSet,
+						&p_pMenu->pSelectionHighlight->HighlightColour,
+						p_X - ( TextLength * 0.5f ),
+						p_Y + ( float )p_pMenu->pGlyphSet->LineHeight *
+							( Index * p_Spacing ),
+						p_pMenu->pMenuItems[ Index ].pName );
+
+					break;
+				}
+			}
+		}
+		else
+		{
+			switch( p_pMenu->MenuItemAlignment )
+			{
+				case MENU_ITEM_ALIGNMENT_LEFT:
+				{
+					TXT_RenderString( p_pMenu->pGlyphSet, &p_pMenu->TextColour,
+						p_X, p_Y + ( float )p_pMenu->pGlyphSet->LineHeight *
+							( Index * p_Spacing ),
+						p_pMenu->pMenuItems[ Index ].pName );
+
+					break;
+				}
+				case MENU_ITEM_ALIGNMENT_RIGHT:
+				{
+					float TextLength;
+
+					TXT_MeasureString( p_pMenu->pGlyphSet,
+						p_pMenu->pMenuItems[ Index ].pName, &TextLength );
+					TXT_RenderString( p_pMenu->pGlyphSet, &p_pMenu->TextColour,
+						p_X - TextLength,
+						p_Y + ( float )p_pMenu->pGlyphSet->LineHeight *
+							( Index * p_Spacing ),
+						p_pMenu->pMenuItems[ Index ].pName );
+
+					break;
+				}
+				case MENU_ITEM_ALIGNMENT_CENTRE:
+				{
+					float TextLength;
+
+					TXT_MeasureString( p_pMenu->pGlyphSet,
+						p_pMenu->pMenuItems[ Index ].pName, &TextLength );
+					TXT_RenderString( p_pMenu->pGlyphSet, &p_pMenu->TextColour,
+						p_X - ( TextLength * 0.5f ),
+						p_Y + ( float )p_pMenu->pGlyphSet->LineHeight *
+							( Index * p_Spacing ),
+						p_pMenu->pMenuItems[ Index ].pName );
+
+					break;
+				}
+			}
+		}
 	}
 }
 
